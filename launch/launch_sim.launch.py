ee8d503ca40c1023e713688f70669378d76754ad
Adesola Samuel <adesolasamuel2018@gmail.com>
2024-08-09 13:26:58 +0100

    V2 Done

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -38,12 +38,24 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'Genesis'],
                         output='screen')
+    
+    diff_drive_spawner = Node(
+                                package="controller_manager",
+                                executable="spawner",
+                                arguments=["diff_cont"],
+                                )
 
-
+    joint_broad_spawner = Node(
+                                package="controller_manager",
+                                executable="spawner",
+                                arguments=["joint_broad"],
+                                )
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
